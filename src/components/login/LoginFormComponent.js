98852c2ebde593b8413127750b71dca7a2480665
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-04-05 14:58:39 +0200

    Cleaner styling on loginform

--- src/components/login/LoginFormComponent.js
+++ src/components/login/LoginFormComponent.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import LinkedStateMixin from 'react-addons-linked-state-mixin';
 import reactMixin from 'react-mixin';
-import {Button, Input, Alert, Glyphicon} from 'react-bootstrap';
+import {ButtonInput, Input, Alert, Glyphicon} from 'react-bootstrap';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actionCreators from '../../actions';
@@ -28,14 +28,14 @@ class LoginFormComponent extends React.Component {
     const innerUserIcon = <Glyphicon bsClass="glyphicon usericon" glyph="user" />;
     const innerLockIcon = <Glyphicon bsClass="glyphicon lockicon" glyph="lock" />;
     return (
-        <form className="login-form">
+        <form className="login-form" onSubmit={this.login.bind(this)}>
         {this.props.statusText
          ? <Alert bsStyle="danger">{this.props.statusText}</Alert>
          : null
         }
           <Input type="text" placeholder="Brukernavn" className="username" valueLink={this.linkState('username')} addonBefore={innerUserIcon} />
         <Input type="password" placeholder="Passord" className="password" valueLink={this.linkState('password')} addonBefore={innerLockIcon} />
-          <Button bsType="submit" bsStyle="danger" block disabled={this.props.isAuthenticating} onClick={this.login.bind(this)}>Logg inn</Button>
+          <ButtonInput type="submit" className="form-control" bsStyle="danger" block disabled={this.props.isAuthenticating} value="Logg inn" />
         </form>
     );
   }
