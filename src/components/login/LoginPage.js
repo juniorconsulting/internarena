14db3d2a062340b6096427a173c77a7bdd742284
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-04-22 18:45:50 +0200

    Configure sensible linting rules, cleanup errors

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,5 +1,3 @@
-//require('normalize.css/normalize.css');
-
 import React from 'react';
 import {Col, Grid, Row} from 'react-bootstrap';
 import LoginFormComponent from './LoginFormComponent';
@@ -8,25 +6,23 @@ let jrcLogo = require('../../images/jr_white.svg');
 require('../../styles/login/LoginPage.scss');
 
 class LoginPage extends React.Component {
-    render() {
-        return (
-            <div className="login-container">
-                <Grid>
-                    <Col sm={6} smOffset={3} md={4} mdOffset={4} xs={8} xsOffset={2} className="login-form">
-                        <Row>
-                            <img src={jrcLogo} alt="JrC Logo" className="img-responsive" id="logo" />
-                        </Row>
-                        <Row>
-                            <LoginFormComponent next={this.props.location.query.next || '/'}/>
-                        </Row>
-                    </Col>
-                </Grid>
-            </div>
-        );
-    }
-}
 
-LoginPage.defaultProps = {
-};
+  render() {
+    return (
+      <div className="login-container">
+        <Grid>
+          <Col sm={6} smOffset={3} md={4} mdOffset={4} xs={8} xsOffset={2} className="login-form">
+            <Row>
+              <img src={jrcLogo} alt="JrC Logo" className="img-responsive" id="logo" />
+            </Row>
+            <Row>
+              <LoginFormComponent next={this.props.location.query.next || '/'}/>
+            </Row>
+          </Col>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
-import { dispatch } from 'redux';
+import {Route} from 'react-router';
 
 // Components
 import App from './components/App';
@@ -8,13 +7,13 @@ import Home from './components/Home';
 import LoginPage from './components/login/LoginPage';
 import {requireAuthentication} from './components/AuthenticatedComponent';
 
-require('styles/Main.scss')
+require('styles/Main.scss');
 
 export default (
-    <div>
-      <Route component={App}>
-        <Route component={requireAuthentication(Home)} path="/" />	
-        <Route component={LoginPage} path="/login" />
-      </Route>
-    </div>
+  <div>
+    <Route component={App}>
+      <Route component={requireAuthentication(Home)} path="/" />
+      <Route component={LoginPage} path="/login" />
+    </Route>
+  </div>
 );
