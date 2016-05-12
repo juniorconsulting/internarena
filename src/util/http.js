bc9cc547ad60e67d9c3fe853349c8967d0c4cfe2
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-05-12 13:30:25 +0200

    Add camelizeProps util

--- src/util/http.js
+++ src/util/http.js
@@ -1,6 +1,6 @@
 export function checkStatus(response) {
   if (response.status < 200 || response.status > 300) {
-    var error = new Error(response.statusText);
+    var error = new Error(response.statÆ_usText);
     error.response = response;
     throw error;
   }
@@ -9,4 +9,22 @@ export function checkStatus(response) {
 
 export function parseJSON(response) {
   return response.json();
+}
+
+export function camelizeProps(json, delimiter = "_") {
+  let newObj = {};
+  for (var key in json) {
+    if (!json.hasOwnProperty(key)) {
+      continue;
+    }
+    let split = key.split(delimiter);
+    let newKey = split[0];
+    if (split.length > 1) {
+      for (var word of split.slice(1, split.length + 1)) {
+        newKey += word.charAt(0).toUpperCase();
+      }
+    }
+    newObj[newKey] = json[key];
+  }
+  return newObj;
 }
---
+++ test/util/http.js
@@ -0,0 +1,37 @@
+import expect from 'expect';
+
+import {camelizeProps} from '../../src/util/http.js';
+
+describe('camelizeProps', () => {
+  it('should remove underscores and capitalize letters following them', () => {
+    const underscored = {
+      'a_b': 1
+    };
+    const camelized = {
+      aB: 1
+    };
+    expect(camelizeProps(underscored)).toEqual(camelized);
+  });
+
+  it('should preserve single word keys', () => {
+    const orig = {
+      'a': 1,
+      'a_b': 2
+    };
+    const camelized = {
+      a: 1,
+      aB: 2
+    };
+    expect(camelizeProps(orig)).toEqual(camelized);
+  });
+
+  it('should work for any delimiter', () => {
+    const orig = {
+      'a-b': 1
+    };
+    const camelized = {
+      aB: 1
+    };
+    expect(camelizeProps(orig, "-")).toEqual(camelized);
+  });
+});
