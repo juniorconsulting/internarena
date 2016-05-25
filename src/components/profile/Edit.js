9c364030ac4a1e0940fef7f9e7106cc356d937c9
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-05-25 21:33:59 +0000

    Tweaks, add some more form controls

--- src/components/profile/Edit.js
+++ src/components/profile/Edit.js
@@ -4,18 +4,45 @@ import * as actionCreators from '../../actions';
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {Button} from 'react-bootstrap';
+import {Button, Input} from 'react-bootstrap';
 
 class Edit extends React.Component {
+
   logout() {
     this.props.actions.logoutUser(this.props.token);
   }
 
+  handleChange(e) {
+    console.log(e.target.value);
+  }
+
   render() {
+    //    var titleComponent =
+    //          (
+    //            <FormGroup controlId="formControlSelect">
+    //              <ControlLabel>Title</ControlLabel>
+    //              <FormControl componentClass="select">
+    //                <option value="consultant">Consultant</option>
+    //                <option value="partner">Partner</option>
+    //              </FormControl>
+    //            </FormGroup>
+    //          );
     return (
       <div className="profile-container" id="edit-profile-form">
         <h2 className="heading">INFO</h2>
         <form>
+          <Input
+             type="text"
+             onChange={this.handleChange}
+             value={this.props.profile.firstName} />
+          <Input
+             type="text"
+             onChange={this.handleChange}
+             value={this.props.profile.lastName} />
+          <select value={this.props.profile.title} onChange={this.handleChange}>
+            <option value="CONSULTANT">Consultant</option>
+            <option value="PARTNER">Partner</option>
+          </select>
           <div className="toggle-container">
             <p>Active</p>
             <div className="switch">
