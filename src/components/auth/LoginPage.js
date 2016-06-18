0820544efaa3f64d7f86d83be488437ee14755a9
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-06-18 15:01:13 +0200

    Add AuthPage
    The RegisterPage and the LoginPage share a lot of styling and
    structure. The generic AuthPage abstracts this away, and renders the
    form specified in the form-prop passed to it.

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import {Col, Grid, Row} from 'react-bootstrap';
 import LoginFormComponent from './LoginFormComponent';
-
-let jrcLogo = require('../../images/jr_white.svg');
-require('../../styles/login/LoginPage.scss');
+import AuthPage from './AuthPage';
 
 class LoginPage extends React.Component {
 
   render() {
     return (
-      <div className="login-container">
-        <Grid>
-          <Col sm={6} smOffset={3} md={4} mdOffset={4} xs={8} xsOffset={2} className="login-form">
-            <Row>
-              <img src={jrcLogo} alt="JrC Logo" className="img-responsive" id="logo" />
-            </Row>
-            <Row>
-              <LoginFormComponent next={this.props.location.query.next || '/'}/>
-            </Row>
-          </Col>
-        </Grid>
-      </div>
+      <AuthPage form={LoginFormComponent} next={this.props.location.query.next || '/'} />
     );
   }
 }
--- src/components/auth/RegisterPage.js
+++ src/components/auth/RegisterPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AuthPage from './AuthPage';
+
+require('../../styles/login/LoginPage.scss');
 
 class RegisterPage extends React.Component {
 
   render() {
     return (
-      <p>Register</p>
+      <AuthPage next="/" />
     );
   }
 }
